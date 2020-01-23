542038b5568835dae36ec3ca985bed43f1b2dd12
zachnuss <57368628+zachnuss@users.noreply.github.com>
2020-01-22 22:14:16 -0800

    Add files via upload

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Chunk", menuName ="Chunk")]
+public class Chunk : ScriptableObject
+{
+    public Basic_Block_List chunkBuildingBlocks;
+    [Header("Chunk Left Down Corner Coords")]
+    public int Xposition;
+    public int Zposition;
+    [Header("Chunk Array Locator (X,Z,Y)")]
+    public GameObject[ , , ] chunkBlocks;
+
+    
+
+    public void createFlatChunk( int maxHight,int startX,int startZ)
+    {
+        //chunks always are the same size 16x16x256
+        chunkBlocks = new GameObject[16,16,256];
+        //ints to keep track of loop position
+        int X, Y, Z;
+        for (Y = 0; Y < maxHight; Y++)
+        {
+            for ( X = 0; X < startX +16; X++)
+            {
+                for ( Z = 0; Z < 16; Z++)
+                {
+                    //creating block in the space
+                    GameObject newBlock = null;
+                    
+                    if(Y == maxHight - 1)
+                    {
+                        newBlock = Instantiate(chunkBuildingBlocks.basicBlocks[0]);
+                    }
+                    else if(Y >= maxHight - 4)
+                    {
+                        newBlock = Instantiate(chunkBuildingBlocks.basicBlocks[1]);
+                    }
+                    else if (Y < maxHight - 4)
+                    {
+                        newBlock = Instantiate(chunkBuildingBlocks.basicBlocks[2]);
+                    }
+                    
+                    chunkBlocks[X, Z, Y] = newBlock;
+                    newBlock.transform.position = new Vector3(startX+X, Y, startZ+Z);
+                    newBlock.GetComponent<_Block>().setFacesFalse();
+                    if (X == 0)
+                    {
+                        newBlock.GetComponent<_Block>().xNegVisable();
+                    }
+                    if (X == 15)
+                    {
+                        newBlock.GetComponent<_Block>().xPosVisable();
+                    }
+                    if (Z == 0)
+                    {
+                        newBlock.GetComponent<_Block>().zNegVisable();
+                    }
+                    if (Z == 15)
+                    {
+                        newBlock.GetComponent<_Block>().zPosVisable();
+                    }
+                    if (Y == 0)
+                    {
+                        newBlock.GetComponent<_Block>().bottemVisable();
+                    }
+                    if (Y == maxHight-1)
+                    {
+                        newBlock.GetComponent<_Block>().topVisable();
+                    }
+                    if (!newBlock.GetComponent<_Block>().areFacesActive())
+                    {
+                        newBlock.SetActive(false);
+                    }
+
+
+
+                }
+               
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/_Block.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Block : MonoBehaviour
+{
+    [Header("All Block GameObject Pieces")]
+    [Tooltip("Complete block")]
+    public GameObject block;
+    [Tooltip("Block faces")]
+    public GameObject Ypos,Yneg,Xpos,Xneg,Zpos,Zneg;
+    [Header("Block Variables")]
+    [Tooltip("Amount of time in Seconds(Modified by pick axe)")]
+    public float breakSpeed;
+
+    //sets certain faces active
+    public void topVisable()
+    {
+        Ypos.SetActive(true);
+    }
+    public void bottemVisable()
+    {
+        Yneg.SetActive(true);
+    }
+    public void xPosVisable()
+    {
+        Xpos.SetActive(true);
+    }
+    public void xNegVisable()
+    {
+        Xneg.SetActive(true);
+    }
+    public void zPosVisable()
+    {
+        Zpos.SetActive(true);
+    }
+    public void zNegVisable()
+    {
+        Zneg.SetActive(true);
+    }
+    //sets all faces inactive
+    public void setFacesFalse()
+    {
+        Ypos.SetActive(false);
+        Yneg.SetActive(false);
+        Xpos.SetActive(false);
+        Xneg.SetActive(false);
+        Zpos.SetActive(false);
+        Zneg.SetActive(false);
+    }
+    //returns false if all faces are false
+    public bool areFacesActive()
+    {
+        if (Ypos.activeSelf == false && Yneg.activeSelf == false && Xpos.activeSelf == false && Xneg.activeSelf == false &&
+            Zpos.activeSelf == false && Zneg.activeSelf == false)
+            return false;
+        else
+            return true;
+        
+       
+    }
+
+   
+}
